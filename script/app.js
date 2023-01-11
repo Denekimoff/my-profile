4d7832be9c5f125fe9410a5e8a32badaac9aaadc
Ekimov  Denis <ekimoffdenis@gmail.com>
2023-01-11 20:01:58 +0300

    fix scroll

--- script/app.js
+++ script/app.js
@@ -3,19 +3,39 @@ const burgerLineElement = document.querySelector('.header__burger-line')
 const menuElement = document.querySelector('.header__menu')
 const menuLinksCollection = document.querySelectorAll('.header__item')
 
-const handlerClickBurger = () => {
+const aboutSectionElement = document.querySelector('.main__about')
+const summarySectionElement = document.querySelector('.main__summary')
+const skillsSectionElement = document.querySelector('.main__skills')
+const linksSectionElement = document.querySelector('.main__links')
+const educationSectionElement = document.querySelector('.main__education')
+
+const linksObject = {
+  Main: aboutSectionElement,
+  Summary: summarySectionElement,
+  Skills: skillsSectionElement,
+  Links: linksSectionElement,
+  Education: educationSectionElement,
+}
+
+const handleClickBurger = () => {
   document.body.classList.toggle('stop-scroll')
   burgerElement.classList.toggle('header__burger--active')
   menuElement.classList.toggle('header__menu--visible')
 }
 
-const handlerClickOnMenuLinks = () => {
+const handleClickOnMenuLinks = event => {
+  const prop = event.target.innerHTML
+
   document.body.classList.remove('stop-scroll')
   burgerElement.classList.toggle('header__burger--active')
   menuElement.classList.toggle('header__menu--visible')
+
+  if (linksObject[prop]?.tagName == 'SECTION') {
+    linksObject[prop].scrollIntoView({ block: 'center', behavior: 'smooth' })
+  }
 }
 
-const handlerClickOuterMenu = event => {
+const handleClickOuterMenu = event => {
   if (burgerElement.contains(event.target)) return
   if (!menuElement.classList.contains('header__menu--visible')) return
   if (menuElement.contains(event.target)) return
@@ -25,7 +45,7 @@ const handlerClickOuterMenu = event => {
 }
 
 menuLinksCollection.forEach(link =>
-  link.addEventListener('click', handlerClickOnMenuLinks)
-  )
-burgerElement.addEventListener('click', handlerClickBurger)
-document.body.addEventListener('click', handlerClickOuterMenu)
+  link.addEventListener('click', handleClickOnMenuLinks)
+)
+burgerElement.addEventListener('click', handleClickBurger)
+document.body.addEventListener('click', handleClickOuterMenu)
